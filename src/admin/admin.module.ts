741fa4b84e4b2805eb14c0de85c8b0ae13b2223e
iamjailan <ahmadjailan2012@gmail.com>
2024-08-31 19:49:35 +0430

    created me route for admin

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -4,10 +4,11 @@ import { AdminController } from './admin.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { PasswordService } from 'src/GlobalService/password.service';
 import { AuthModule } from './auth/auth.module';
+import { MeModule } from './me/me.module';
 
 @Module({
   controllers: [AdminController],
   providers: [AdminService, PrismaService, PasswordService],
-  imports: [AuthModule],
+  imports: [AuthModule, MeModule],
 })
 export class AdminModule {}
